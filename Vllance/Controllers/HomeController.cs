0dd43b38470e565667b030e946b70571559b0376
tamimrahman <tamim101rahman@gmail.com>
2025-10-29 15:34:41 +0600

    integrated yolo-based fastapi to the frontend

--- Vllance/Controllers/HomeController.cs
+++ Vllance/Controllers/HomeController.cs
@@ -17,6 +17,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Demo()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
